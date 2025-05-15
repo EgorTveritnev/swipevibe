5f1054023b54846c26f0da684d3b55cebde4b26d
VadimSiminenco <vadim.siminenco@isa.utm.md>
2025-05-15 11:11:10 +0300

    Filters

---
+++ SwipeVibe.Web/Filters/AdminOnlyAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SwipeVibe.Web.Filters
+{
+    public class AdminOnlyAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var session = httpContext.Session;
+            if (session == null) return false;
+
+            var role = session["Role"]?.ToString();
+            return role == "Admin";
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("/Home/AccessDenied");
+        }
+    }
+}
---
+++ SwipeVibe.Web/Filters/UserOnlyAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SwipeVibe.Web.Filters
+{
+    public class UserOnlyAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var session = httpContext.Session;
+            if (session == null) return false;
+
+            var role = session["Role"]?.ToString();
+            return role == "User";
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("/Home/AccessDenied");
+        }
+    }
+}
